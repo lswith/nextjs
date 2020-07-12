f906f41479ff2a22b9704b354478ef8803d95136
alex leon <alex@leon.st>
2020-07-13 00:20:22 +1000

    Fix typescript error

--- components/FormattedContent/index.tsx
+++ components/FormattedContent/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.css';
 import cx from 'classnames';
 
-export default ({ children, className }) => (
+export default ({ children, className }: { children: any, className?: string }) => (
   <div className={cx(styles.formattedContent, className)}>
     {children}
   </div>
