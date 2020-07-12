ff7d79263dc70fbcd15a1e3f7a775db929cdfc91
Luke Swithenbank Personal <lswith2@Lukes-MacBook-Pro.local>
2020-07-12 22:27:42 +1000

    add login, callback api calls

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -1,25 +1,49 @@
 import querystring from 'querystring';
+import cookie from 'cookie';
 import request from 'request';
 
-const stateKey = 'spotify_auth_state';
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-const redirect_uri = process.env.REDIRECT_URL;
+const redirect_uri = process.env.REDIRECT_URL
+const stateKey = 'spotify_auth_state';
 
-export default (req, res) => {
+const ResetCookieSerialized = cookie.serialize(stateKey, '', {
+    sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: -1,
+    httpOnly: true,
+    path: '/',
+  })
+
+function serializeCookie(name, value) {
+   return cookie.serialize(name, value, {
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 72576000,
+      httpOnly: true,
+      path: '/',
+  });
+}
+
+export default async (req, res) => {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+  
     const code = req.query.code || null;
     const state = req.query.state || null;
     const storedState = req.cookies ? req.cookies[stateKey] : null;
   
     if (state === null || state !== storedState) {
-      res.redirect(
-        '/#' +
+      res.writeHead(302, { Location: '/error' +
+        +
           querystring.stringify({
             error: 'state_mismatch',
           })
-      );
+      });
+      res.end();
     } else {
-      res.clearCookie(stateKey);
+      res.setHeader('Set-Cookie', ResetCookieSerialized);
+      
       const authOptions = {
         url: 'https://accounts.spotify.com/api/token',
         form: {
@@ -52,21 +76,18 @@ export default (req, res) => {
           });
   
           // we can also pass the token to the browser to make requests from there
-          res.redirect(
-            '/#' +
-              querystring.stringify({
-                access_token: access_token,
-                refresh_token: refresh_token,
-              })
-          );
+
+          res.setHeader('Set-Cookie', serializeCookie('access_token', access_token));
+          res.setHeader('Set-Cookie', serializeCookie('refresh_token', refresh_token));
+          res.writeHead(302, { Location: '/' });
+          res.end();
         } else {
-          res.redirect(
-            '/#' +
+          res.writeHead(302, { Location: '/error' +
               querystring.stringify({
                 error: 'invalid_token',
               })
-          );
+            });
         }
       });
     }
-}
+  }
