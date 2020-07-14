c89db03b6ce0f9a0c828d0cd2c87e47afc81cf81
Luke Swithenbank <swithenbank.luke@gmail.com>
2020-07-14 11:07:03 +1000

    use hooks instead

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import cookie from 'cookie';
-import { Component } from 'react';
+import { useState } from 'react';
 
 import Page from '../components/Page';
 import Button from '../components/Button';
@@ -17,119 +17,95 @@ const pluralize = (count, word) => {
   return `${count} ${word}${count === 1 ? '' : 's'}`;
 }
 
-interface DashboardProps {
-  profilePhoto: string
-  name: string
-  artistCount: number
-  playlistCount: number
-  songCount: number
-  albumsCount: number
-  token: string
+export default function Dashboard(props) {
+  const [artistCount, setAristCount] = useState(props.artistCount);
+  const [playlistCount, setPlaylistCount] = useState(props.playlistCount);
+  const [songCount, setSongCount] = useState(props.songCount);
+  const [albumsCount, setAlbumsCount] = useState(props.albumsCount);
+
+  const removeArtists = () => {
+    UnFollowArtists(props.token);
+    setAristCount(0);
+  }
+
+  const removePlaylists = () => {
+    UnfollowPlaylists(props.token);
+    setPlaylistCount(0);
+  }
+
+  const removeSongs = () => {
+    RemoveTracksFromLibrary(props.token);
+    setSongCount(0);
+  }
+
+  const removeAlbums = () => {
+    RemoveAlbumsFromLibrary(props.token);
+    setAlbumsCount(0);
+  }
+
+  const removeAll = () => {
+    removeArtists();
+    removePlaylists();
+    removeSongs();
+    removeAlbums();
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Spotify r4set</title>
+      </Head>
+      <Page>
+        <div className={styles.avatarContainer}>
+          <img className={styles.img} src={props.profilePhoto} />
+          <h1>{props.name}</h1>
+        </div>
+
+        <Button onClick={removeAll} className={styles.resetButton}>Reset all my spotify!!!</Button>
+
+        <FormattedContent>
+          <p className={styles.resetText}>Resetting all your spotify will:</p>
+
+          <ul>
+            <li className={styles.listItem}>
+              <Button onClick={removeArtists}>Unfollow all artists</Button>
+              <span className={styles.meta}>You are currently following {pluralize(artistCount, 'artist')}</span>
+            </li>
+            <li className={styles.listItem}>
+              <Button onClick={removePlaylists}>Unfollow all playlists</Button>
+              <span className={styles.meta}>You are currently following {pluralize(playlistCount, 'playlist')}</span>
+            </li>
+            <li className={styles.listItem}>
+              <Button onClick={removeSongs}>Unlike all songs</Button>
+              <span className={styles.meta}>You are currently liking {pluralize(songCount, 'song')}</span>
+            </li>
+            <li className={styles.listItem}>
+              <Button onClick={removeAlbums}>Unlike all albums</Button>
+              <span className={styles.meta}>You are currently liking {pluralize(albumsCount, 'album')}</span>
+            </li>
+          </ul>
+        </FormattedContent>
+      </Page>
+    </>
+  )
 }
 
-interface DashboardState {
-  artistCount: number
-  playlistCount: number
-  songCount: number
-  albumsCount: number
-}
+Dashboard.getInitialProps = async (ctx) => {
+  const cookies = cookie.parse(ctx.req.headers.cookie);
+  const token = cookies.access_token;
+  const artistCount = await GetFollowedArtistsCount(token);
+  const playlistsCount = await GetPlaylistsCount(token);
+  const songsCount = await GetLibraryTrackCount(token);
+  const albumsCount = await GetLibraryAlbumsCount(token);
+  const me = await GetMe(token);
 
-export default class Dashboard extends Component<DashboardProps, DashboardState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      artistCount: this.props.artistCount,
-      songCount: this.props.songCount,
-      playlistCount: this.props.playlistCount,
-      albumsCount: this.props.albumsCount
-    };
-  }
-
-  static async getInitialProps(ctx) {
-    const cookies = cookie.parse(ctx.req.headers.cookie);
-    const token = cookies.access_token;
-    const artistCount = await GetFollowedArtistsCount(token);
-    const playlistsCount = await GetPlaylistsCount(token);
-    const songsCount = await GetLibraryTrackCount(token);
-    const albumsCount = await GetLibraryAlbumsCount(token);
-    const me = await GetMe(token);
-
-    return {
-      profilePhoto: me.images?.length > 0 ? me.images[0].url : './default-profile.png',
-      name: me.display_name,
-      artistCount: artistCount,
-      playlistCount: playlistsCount,
-      songCount: songsCount,
-      albumsCount: albumsCount,
-      token,
-    }
-  }
-
-  removeArtists = () => {
-    UnFollowArtists(this.props.token);
-    this.setState({ artistCount: 0 })
-  }
-
-  removePlaylists = () => {
-    UnfollowPlaylists(this.props.token);
-    this.setState({ playlistCount: 0 })
-  }
-
-  removeSongs = () => {
-    RemoveTracksFromLibrary(this.props.token);
-    this.setState({ songCount: 0 })
-  }
-
-  removeAlbums = () => {
-    RemoveAlbumsFromLibrary(this.props.token);
-    this.setState({ albumsCount: 0 })
-  }
-
-  removeAll = () => {
-    this.removeArtists()
-    this.removePlaylists()
-    this.removeSongs()
-    this.removeAlbums()
-  }
-
-  render() {
-    return (
-      <>
-        <Head>
-          <title>Spotify r4set</title>
-        </Head>
-        <Page>
-          <div className={styles.avatarContainer}>
-            <img className={styles.img} src={this.props.profilePhoto} />
-            <h1>{this.props.name}</h1>
-          </div>
-
-          <Button onClick={this.removeAll} className={styles.resetButton}>Reset all my spotify!!!</Button>
-
-          <FormattedContent>
-            <p className={styles.resetText}>Resetting all your spotify will:</p>
-
-            <ul>
-              <li className={styles.listItem}>
-                <Button onClick={this.removeArtists}>Unfollow all artists</Button>
-                <span className={styles.meta}>You are currently following {pluralize(this.state.artistCount, 'artist')}</span>
-              </li>
-              <li className={styles.listItem}>
-                <Button onClick={this.removePlaylists}>Unfollow all playlists</Button>
-                <span className={styles.meta}>You are currently following {pluralize(this.state.playlistCount, 'playlist')}</span>
-              </li>
-              <li className={styles.listItem}>
-                <Button onClick={this.removeSongs}>Unlike all songs</Button>
-                <span className={styles.meta}>You are currently liking {pluralize(this.state.songCount, 'song')}</span>
-              </li>
-              <li className={styles.listItem}>
-                <Button onClick={this.removeAlbums}>Unlike all albums</Button>
-                <span className={styles.meta}>You are currently liking {pluralize(this.state.albumsCount, 'album')}</span>
-              </li>
-            </ul>
-          </FormattedContent>
-        </Page>
-      </>
-    )
+  return {
+    profilePhoto: me.images?.length > 0 ? me.images[0].url : './default-profile.png',
+    name: me.display_name,
+    artistCount: artistCount,
+    playlistCount: playlistsCount,
+    songCount: songsCount,
+    albumsCount: albumsCount,
+    token,
   }
 }
